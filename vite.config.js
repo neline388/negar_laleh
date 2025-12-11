76e3c81364002444480a0bc6d2a6b1f423b9be4c
neline388 <negarlaleh1@gmail.com>
2025-12-11 12:59:12 +0100

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/protfolio_react/",
+  base: "/portfolio_react/",
   plugins: [react()],
 })
